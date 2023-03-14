fba6c38b1400e961ca3cc3390725038117e1abd3
Taiki0807 <hosokawataiki265@gmail.com>
2023-03-14 17:21:44 +0900

    [add] Add image

--- account/serializers.py
+++ account/serializers.py
@@ -7,12 +7,13 @@ class AccountSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Account
-        fields = ['username', 'password']
+        fields = ['username', 'password','image']
 
     def create(self, validated_data):
         user = Account(
             username=validated_data['username'],
         )
         user.set_password(validated_data['password'])
+        user.image = validated_data.get('image')
         user.save()
         return user
